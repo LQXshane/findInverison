5c94c65080fbdd87a007581be102dc1c02586dcc
linqiuxuan <linqiuxuan@dianping.com>
2016-09-12 23:20:49 -0400

    first run

--- findInversion.py
+++ findInversion.py
@@ -1,13 +1,29 @@
 # qiuxuan.lin
 
 import string
+import datetime
 
-
-text = []
+a = [] # list of rankings
 with open('hw1test.txt') as file:
     for line in file:
         tmp = line.strip(string.whitespace)
-        text.append(int(tmp))
+        a.append(int(tmp))
+
+n = len(a)
+inversion = 0 # of large inversions
+
+print datetime.datetime.now()
+# naive search
+for i in range(0,n,1):
+    for j in range(0,n,1):
+        if i < j:
+            if a[i]>2 * a[j] :
+                inversion = inversion + 1
+
+print datetime.datetime.now()
+
+
+
 
 
 
